98d26e0bdf635f96a442b970cb8df53e75740bbb
Gagskaya <alisxan006@mail.ru>
2021-03-24 16:06:48 +0300

    Updates

---
+++ precache-manifest.f6c33ea445b452c59085b58ff5ef9902.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f24589e7d0e7ef26a702ea70c1a7eed",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e11e5cf78011377a1706",
+    "url": "/todo/static/css/main.3c10c76c.chunk.css"
+  },
+  {
+    "revision": "e01f620e441a5aa821c4",
+    "url": "/todo/static/js/2.d63b0def.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/todo/static/js/2.d63b0def.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e11e5cf78011377a1706",
+    "url": "/todo/static/js/main.07e848db.chunk.js"
+  },
+  {
+    "revision": "ac43cb8665000a809aeb",
+    "url": "/todo/static/js/runtime-main.8d8a3af4.js"
+  }
+]);
